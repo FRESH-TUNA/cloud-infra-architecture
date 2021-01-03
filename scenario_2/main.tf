1942166e1eb652d2849880d9a95ad4c081347b18
lunacircle4 <lunacircle4@likelion.org>
2021-01-04 02:05:15 +0900

    feat: scenario_2 refactor

--- scenario_2/main.tf
+++ scenario_2/main.tf
@@ -15,27 +15,27 @@ module "vpc" {
 }
 
 module "logic_eip" {
-  source = "./eip/logic"
+  source = "./eip"
 }
 
 module "logic_key" {
-  source = "./key/logic"
+  source = "./key"
 }
 
 module "logic_sg" {
-  source = "./sg/logic"
+  source = "./sg"
   vpc_id = module.vpc.id
 }
 
 module "logic_launch_template" {
-  source = "./launch_template/logic"
+  source = "./launch_template"
   key_name = module.logic_key.key_name
   eip_allocation_id = module.logic_eip.allocation_id
   vpc_security_group_ids = [module.logic_sg.id]
 }
 
 module "logic_asg" {
-  source = "./asg/logic"
+  source = "./asg"
   aws_launch_template_id = module.logic_launch_template.id
   subnet_ids = module.vpc.public_subnet_ids
 }
